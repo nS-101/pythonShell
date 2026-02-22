6cf6ec1819391c03584c2ae9b8b3bbd1fa9ec223
Neil Singh <neilsingh.miami@gmail.com>
2026-02-22 15:08:46 +0000

    [any message]

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    command = input("")
-    #right now, we're treating all inputs as invalid
-    if command:
-        print(f"{command}: command not found")
-    else:
-        print(f"{command}: command not found")
-    pass
+    while True:
+        sys.stdout.write("$ ")
+        command = input("")
+        #right now, we're treating all inputs as invalid
+        if command:
+            print(f"{command}: command not found")
+        else:
+            print(f"{command}: command not found")
+        pass
 
 
 if __name__ == "__main__":
