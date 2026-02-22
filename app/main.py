2619e3740d41075e9327d6c10380eb1867031a49
Neil Singh <neilsingh.miami@gmail.com>
2026-02-22 18:53:35 +0000

    [any message]

--- app/main.py
+++ app/main.py
@@ -5,9 +5,13 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input("")
+        commandArray = command.strip().split()
         #right now, we're treating all inputs as invalid
         if command == "exit":
             break
+        elif commandArray[0] == "echo":
+            commandString = " ".join(commandArray[1:])
+            print(commandString)
         else:
             print(f"{command}: command not found")
         pass
